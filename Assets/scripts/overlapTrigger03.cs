7fa9361ce373905b6b21651fcff1cef938ac4a87
unknown <Jake@DABWDB0602WSPVR.adsroot.uts.edu.au>
2021-04-26 16:39:43 +1000

    added experimentation with different information visualisation. see notes for details on experimentation with object collision

---
+++ Assets/scripts/overlapTrigger03.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overlapTrigger03 : MonoBehaviour
+{
+
+    public Color myColor;
+    public float rFloat = 0.1f;
+    public float rNonFloat = 0.9f;
+
+    public Renderer myRenderer;
+
+
+    bool m_Started;
+    //Layer of which to detect objects set to everything if unsure
+    public LayerMask m_LayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //used to draw Gizmos
+        m_Started = false;
+
+        MyCollisions();
+
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    void MyCollisions()
+    {
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, m_LayerMask);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            Debug.Log("Hit: " + hitColliders[i].name + i);
+            i++;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (m_Started)
+        {
+            Gizmos.DrawWireCube(transform.position, transform.localScale);
+        }
+    }
+}
